7b649e701a58dafe2f73455136bb81f96240434c
Mohit Choudhary <choudhary.mohit2026@gmail.com>
2025-07-19 16:22:25 +0530

    Updated Files for Week 5
    Updated Code and outputs for Microservices Exercise 2

---
+++ Week5 - Microservices/2 Microservices/microservices/account/src/main/java/com/cognizant/account/AccountApplication.java
@@ -0,0 +1,15 @@
+package com.cognizant.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountApplication.class, args);
+
+		//http://localhost:8080/accounts/00987987973432
+	}
+
+}
