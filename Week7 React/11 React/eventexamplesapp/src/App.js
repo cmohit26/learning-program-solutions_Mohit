e5edbb5542a1b083531a1befd67e6cba4c3f9c7b
Mohit Choudhary <choudhary.mohit2026@gmail.com>
2025-07-30 00:11:37 +0530

    Updated Week 7 FIles
    Updated FIles!

---
+++ Week7 React/11 React/eventexamplesapp/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './App.css';
+import CurrencyConverter from './CurrencyConverter';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const sayHello = () => {
+    alert("Hello! Member1");
+  };
+
+  const incrementAndSayHello = () => {
+    increment();
+    sayHello();
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const sayWelcome = (message) => {
+    alert(message);
+  };
+
+  const handleClick = () => {
+    alert("I was clicked");
+  };
+
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={incrementAndSayHello}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={() => sayWelcome("welcome")}>Say welcome</button>
+      <button onClick={handleClick}>Click on me</button>
+
+      <CurrencyConverter />
+    </div>
+  );
+}
+
+export default App;
