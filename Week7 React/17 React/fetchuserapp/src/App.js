b5393470926e3ba6a0843a02d6ce9fa215363b0f
Mohit Choudhary <choudhary.mohit2026@gmail.com>
2025-08-02 17:34:08 +0530

    Updated FIles
    Uploaded Files for Week 7

---
+++ Week7 React/17 React/fetchuserapp/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import Getuser from './Getuser';
+
+function App() {
+  return (
+    <div className="App">
+      <Getuser />
+    </div>
+  );
+}
+
+export default App;
